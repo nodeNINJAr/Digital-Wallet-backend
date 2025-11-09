e88e7d60839d5e3ddf2b7e550bf9a45ebe62d53e
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-11-09 20:35:12 +0600

    set-up-role

--- src/app/modules/auth/auth.routes.ts
+++ src/app/modules/auth/auth.routes.ts
@@ -12,8 +12,8 @@ const router = Router();
 // 
 router.post("/login", AuthControllers.credentialsLogin);
 router.get("/verify", checkVerified(...Object.values(Role)), AuthControllers.verifyUser);
-router.post("/refresh-token", AuthControllers.getNewAccessTokens);
-router.post("/logout", AuthControllers.userLogOut);
+router.post("/refresh-token",checkVerified(...Object.values(Role)), AuthControllers.getNewAccessTokens);
+router.post("/logout",checkVerified(...Object.values(Role)), AuthControllers.userLogOut);
 router.post("/reset-password", validationRequest(recoveryPassZodSchema), checkVerified(...Object.values(Role)), AuthControllers.resetPassword);
 
 export const AuthRoutes = router;
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -15,10 +15,10 @@ const router = Router();
 // **user router **
 router.post("/register",validationRequest(createUserZodSchema), UserControllers.createUser);
 router.patch("/update/:id",checkVerified(...Object.values(Role)),validationRequest(updateUserZodSchema), UserControllers.updateUser);
-router.get("/all", UserControllers.getTransactionsWithUserAndWallet);
-router.get("/agents", UserControllers.getAllAgents);
+router.get("/all",checkVerified(Role.ADMIN), UserControllers.getTransactionsWithUserAndWallet);
+router.get("/agents",checkVerified(Role.USER), UserControllers.getAllAgents);
 // 
-router.get("/", UserControllers.getAllusersAg);
+router.get("/",checkVerified(Role.AGENT), UserControllers.getAllusersAg);
 router.patch("/agent/apply",checkVerified(Role.USER), UserControllers.agentStatusUpdate);
 // **
 
